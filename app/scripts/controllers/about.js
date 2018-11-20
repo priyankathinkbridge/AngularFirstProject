8e27944165e13007e6be69ad7afd535152be2397
priyankathinkbridge <priyanka@thinkbridge.in>
2018-11-20 13:15:47 +0530

    projec file commited

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newangularprojectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newangularprojectApp
+ */
+angular.module('newangularprojectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newangularprojectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newangularprojectApp
+ */
+angular.module('newangularprojectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
