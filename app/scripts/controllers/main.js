11485f28fe6e87ea91b970afaf775c4457988b8a
priyankathinkbridge <priyanka@thinkbridge.in>
2018-11-20 15:55:27 +0530

    loccal storage module added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,19 @@
  * # MainCtrl
  * Controller of the newangularprojectApp
  */
-angular.module('newangularprojectApp')
+/*angular.module('newangularprojectApp')
   .controller('MainCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  });*/
+angular.module('newangularprojectApp')
+.controller('MainCtrl',function($scope, localStorageService){
+  var storageType = localStorageService.getStorageType();
+  console.log(storageType);
+  if(localStorageService.isSupported) {
+    //...
+  }
+})
